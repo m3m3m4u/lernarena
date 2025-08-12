a5c89c68a4e43dfc37fa8a060e4f6b66d50f4b5c
Matthias Gmeiner <m3m3m4u@gmail.com>
2025-08-12 13:37:52 +0200

    flugzeug perfekt

---
+++ Autospiel/questions.js
@@ -0,0 +1,13 @@
+// Fragen-Pool (beliebig erweiterbar)
+window.QUIZ_QUESTIONS = [
+  { prompt:'Du .... Karate', answers:['machen','machst','macht','mache'], correctIndex:1 },
+  { prompt:'Er .... Fußball', answers:['spielst','spielt','spielen','spiele'], correctIndex:1 },
+  { prompt:'Wir .... müde', answers:['sind','seid','bin','bist'], correctIndex:0 },
+  { prompt:'Ich .... ein Buch', answers:['lese','liest','lesen','lest'], correctIndex:0 },
+  { prompt:'Ihr .... schnell', answers:['lauft','laufen','läuft','laufe'], correctIndex:0 },
+  { prompt:'Sie (Plural) .... im Chor', answers:['singen','singt','singe','singst'], correctIndex:0 },
+  { prompt:'Du .... gern Musik', answers:['hörst','höre','hören','hört'], correctIndex:0 },
+  { prompt:'Wir .... heute Pizza', answers:['essen','esst','isst','esse'], correctIndex:0 },
+  { prompt:'Er .... im See', answers:['schwimmt','schwimmen','schwimmst','schwimme'], correctIndex:0 },
+  { prompt:'Ich .... einen Film', answers:['sehe','siehst','sehen','seht'], correctIndex:0 },
+];
